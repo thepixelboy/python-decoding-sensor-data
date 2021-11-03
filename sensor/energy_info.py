d4cb8715488da7121748f1d0074105e1e3e87a24
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-11-03 18:28:47 +0100

    feat: get energy data

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -4,3 +4,10 @@ from house_info import HouseInfo
 class EnergyData(HouseInfo):
     ENERGY_PER_BULB = 0.2
     ENERGY_BITS = 0x0F0
+
+    def _get_energy(self, rec):
+        energy = int(rec, base=16)
+        energy = energy & self.ENERGY_BITS
+        energy = energy >> 4
+
+        return energy
